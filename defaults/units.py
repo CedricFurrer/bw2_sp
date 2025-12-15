337654412d1412cbda23eec574339f5a1348d36f
Furrer Cédric <F80856737@agsad.admin.ch>
2025-12-15 16:16:43 +0100

    Add commented pint scripts

--- defaults/units.py
+++ defaults/units.py
@@ -12,3 +12,22 @@ backward_unit_normalization_mapping_orig: dict = {v: k for k, v in bw2io.units.U
 
 # Merge
 backward_unit_normalization_mapping: dict = dict(**backward_unit_normalization_mapping_orig, **additional_normalization_mapping)
+
+# import pint
+# ureg = pint.UnitRegistry()
+# kg_unit: str = "kg"
+# kg_pint = ureg(kg_unit)
+
+# # Define the unit "kg/ha"
+# kg_per_ha = ureg.kg / ureg.hectare  # kg/ha is kilogram per hectare
+
+# # Define the quantity in kg/ha
+# quantity_kg_per_ha = 100 * kg_per_ha  # Example: 100 kg/ha
+
+# # Convert kg/ha to kg/m²
+# quantity_kg_per_m2 = quantity_kg_per_ha.to(ureg.kg / ureg.meter**2)
+
+# # Print the result
+# print(kg_per_ha)
+# quantity_kg_per_ha.to(ureg.kg / ureg.l)
+# print(f"{quantity_kg_per_ha} is equal to {quantity_kg_per_m2}")
